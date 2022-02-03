2ce8de97ec1e03a73ac54714fe91bb2cb8d56b08
Atlas8981 <sannchesda8981@gmail.com>
2022-02-03 09:20:43 +0700

    Initial commit

---
+++ src/main/java/com/example/demo/controllers/GreetingController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/greeting")
+    public String getGreeting() {
+        return "Something";
+    }
+}
